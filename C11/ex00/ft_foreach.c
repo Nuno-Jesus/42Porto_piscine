244ecbecb685e86666eea5e529773563ca0df6ab
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-09-11 13:33:44 +0100

    Exercise 01 ready to deliver. Also commenting exercise 00 unnecessary functions.

--- C11/ex00/ft_foreach.c
+++ C11/ex00/ft_foreach.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_foreach.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 09:23:23 by ncarvalh          #+#    #+#             */
-/*   Updated: 2022/09/08 09:28:46 by ncarvalh         ###   ########.fr       */
+/*   Updated: 2022/09/11 13:33:00 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+/* void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
@@ -28,15 +28,13 @@ void	ft_putnbr(int nb)
 		n = -n;
 	}
 	if (n < 10)
-	{
 		ft_putchar(n + '0');
-	}
 	else
 	{
 		ft_putnbr(n / 10);
 		ft_putchar(n % 10 + '0');
 	}
-}
+} */
 
 void ft_foreach(int *tab, int length, void(*f)(int))
 {
@@ -46,10 +44,10 @@ void ft_foreach(int *tab, int length, void(*f)(int))
 	while (i < length)
 		f(tab[i++]);
 }
-
+/* 
 int main()
 {
 	int array[] = {4, 3, 1, 7, 4, 8};
 	int size = 6;
 	ft_foreach(array, size, &ft_putnbr);
-}
+} */
--- C11/ex01/ft_map.c
+++ C11/ex01/ft_map.c
@@ -6,22 +6,54 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 10:07:40 by ncarvalh          #+#    #+#             */
-/*   Updated: 2022/09/11 13:20:55 by marvin           ###   ########.fr       */
+/*   Updated: 2022/09/11 13:32:29 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+/* 
 int ft_square(int n)
 {
 	return n * n;
-}
+} */
 
 int *ft_map(int *tab, int length, int(*f)(int))
 {
-	int i;
+	int	i;
+	int	*array;
+	
+	if (tab == NULL || f == NULL)
+		return (NULL);
+	i = 0;
+	array = (int *)malloc(length * sizeof(int));
+	if (array == NULL)
+		return (NULL);
+	while (i < length)
+	{
+		array[i] = f(tab[i]);
+		i++;
+	}
+	return (array);
+}
+/* 
+void ft_print(int *array, int size)
+{
+	if(array == NULL)
+		printf("NULL ARRAY\n");
+	else
+	{
+		for(int i = 0; i < size; i++)
+			printf("%d ", array[i]);
+		printf("\n");
+	}
 }
 
 int main()
 {
 	int array[] = {2, 3, 20, 16, 25};
-	int *new = ft_map(array, 5, &ft_square);
-}
+	int size = 0;
+	
+	int *new = ft_map(array, size, &ft_square);
+	ft_print(new, size);
+} */
