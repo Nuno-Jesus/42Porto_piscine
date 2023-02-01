cc38cd0550f26106af846af8f74558c274ff6a37
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-02-01 23:29:40 +0000

    C12: exercise 04 done.

---
+++ C12/ex04/ft_list.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 22:57:54 by marvin            #+#    #+#             */
+/*   Updated: 2023/02/01 22:57:54 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct	s_list
+{
+	struct s_list	*next;
+	void			*data;
+}				t_list;
+
+t_list	*ft_create_elem(void *data);
+
+t_list	*ft_list_last(t_list *begin_list);
+
+#endif
