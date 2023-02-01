87682ce14a8414b778870b727721274c72bbaf32
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-02-01 23:44:32 +0000

    C12: exercise 06 done.

---
+++ C12/ex06/ft_list.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 22:57:54 by marvin            #+#    #+#             */
+/*   Updated: 2023/02/01 22:57:54 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct	s_list
+{
+	struct s_list	*next;
+	void			*data;
+}				t_list;
+
+t_list	*ft_create_elem(void *data);
+
+void	ft_list_push_front(t_list **begin_list, void *data);
+
+t_list	*ft_list_last(t_list *begin_list);
+
+int		ft_list_size(t_list *begin_list);
+
+void	ft_list_push_back(t_list **begin_list, void *data);
+
+t_list	*ft_list_push_strs(int size, char **strs);
+
+#endif
---
+++ C12/ex06/ft_list_clear.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_clear.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 23:39:16 by marvin            #+#    #+#             */
+/*   Updated: 2023/02/01 23:39:16 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
+{
+	t_list	*aux;
+
+	while (begin_list)
+	{
+		aux = begin_list;
+		begin_list = begin_list->next;
+		if (*free_fct)
+			(*free_fct)(aux->data);
+		free(aux);
+	}
+}
+/* 
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list	*node;
+
+	if (!begin_list)
+		return ;
+	node = ft_create_elem(data);
+	if (!node)
+		return ;
+	if (*begin_list)
+		node->next = *begin_list;
+	*begin_list = node;
+}
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list	*elem;
+
+	elem = malloc(sizeof(t_list));
+	if (!elem)
+		return (NULL);
+	elem->data = data;
+	elem->next = NULL;
+	return (elem);
+}
+
+void	ft_list_print(t_list *list)
+{
+	while (list)
+	{
+		printf("%s->", list->data);
+		list = list->next;
+	}
+	printf("NULL\n");
+}
+
+int main(int argc, char **argv)
+{
+	t_list	*list;
+
+	list = NULL;
+	for (int i = 1; i < argc; i++)
+		ft_list_push_front(&list, argv[i]);
+	ft_list_print(list);
+	ft_list_clear(list, NULL);
+}
+*/
