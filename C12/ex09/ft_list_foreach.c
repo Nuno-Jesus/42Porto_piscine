1ccca3c4249505dbf17c801b70e46f8ce441128a
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-02-02 00:35:15 +0000

    C12: exercise 09 done.

---
+++ C12/ex09/ft_list_foreach.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 00:30:54 by marvin            #+#    #+#             */
+/*   Updated: 2023/02/02 00:30:54 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	while (begin_list)
+	{
+		if (*f)
+			(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
+
+/* 
+void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
+{
+	t_list	*aux;
+
+	while (begin_list)
+	{
+		aux = begin_list;
+		begin_list = begin_list->next;
+		if (*free_fct)
+			(*free_fct)(aux->data);
+		free(aux);
+	}
+}
+
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list	*node;
+
+	if (!begin_list)
+		return ;
+	node = ft_create_elem(data);
+	if (!node)
+		return ;
+	if (*begin_list)
+		node->next = *begin_list;
+	*begin_list = node;
+}
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list	*elem;
+
+	elem = malloc(sizeof(t_list));
+	if (!elem)
+		return (NULL);
+	elem->data = data;
+	elem->next = NULL;
+	return (elem);
+}
+
+void	ft_list_print(t_list *list)
+{
+	while (list)
+	{
+		printf("%s->", list->data);
+		list = list->next;
+	}
+	printf("NULL\n");
+}
+
+void	func(void *data)
+{
+	((char *)data)[0] += 1;
+}
+
+int main(int argc, char **argv)
+{
+	t_list	*list;
+
+	list = NULL;
+	for (int i = 1; i < argc; i++)
+		ft_list_push_front(&list, argv[i]);
+	ft_list_print(list);
+	ft_list_foreach(list, &func);
+	ft_list_print(list);
+	ft_list_clear(list, NULL);
+}
+ */
